cf154774deaf188f6a9ecd7dec45b62bc2ff50b4
Luke Massetti <lukemassetti@WestSide-Luke.local>
2018-05-03 17:21:45 -0700

    first file push

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+const MySQL = require('mysql');
+
+// Set up MySQL connection.
+const mysql = require("mysql");
+
+let connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id: " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE table burgers(
+id int AUTO_INCREMENT NOT NULL,
+burger_name VARCHAR (40),
+devoured BOOLEAN NOT NULL,
+PRIMARY KEY (id)
+);
